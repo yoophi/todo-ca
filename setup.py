6f4b4b84c92bbf0ec2f00f9f1fc3cd56f401af74
Pyunghyuk Yoo <yoophi@gmail.com>
2021-01-20 22:01:27 +0900

    fix: apply code formatting with black

--- setup.py
+++ setup.py
@@ -4,56 +4,60 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    'Click>=7.0',
-    'ca_util',
-    'flask',
-    'flask-cors',
-    'flask-marshmallow',
+    "Click>=7.0",
+    "ca_util",
+    "flask",
+    "flask-cors",
+    "flask-marshmallow",
 ]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Pyunghyuk Yoo",
-    author_email='yoophi@gmail.com',
-    python_requires='>=3.5',
+    author_email="yoophi@gmail.com",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
     entry_points={
-        'console_scripts': [
-            'todo_ca=todo_ca.cli:main',
+        "console_scripts": [
+            "todo_ca=todo_ca.cli:main",
         ],
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='todo_ca',
-    name='todo_ca',
-    packages=find_packages(include=['todo_ca', 'todo_ca.*']),
+    keywords="todo_ca",
+    name="todo_ca",
+    packages=find_packages(include=["todo_ca", "todo_ca.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/yoophi/todo_ca',
-    version='0.1.0',
+    url="https://github.com/yoophi/todo_ca",
+    version="0.1.0",
     zip_safe=False,
 )
--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@ import os
 
 from todo_ca.rest import create_app
 
-config = os.environ.get('FLASK_CONFIG', 'default')
+config = os.environ.get("FLASK_CONFIG", "default")
 app = create_app(config)
